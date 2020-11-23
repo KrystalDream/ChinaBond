902dd04699ac0d0b3704510e31c45d3b6b1eec3b
862176118@qq.com <Shaomeng123456>
2020-11-23 16:10:02 +0800

    头文件 屏幕适配

--- ChinaBond/CBFunction.h
+++ ChinaBond/CBFunction.h
@@ -5,13 +5,40 @@
 //  Created by wangran on 15/11/30.
 //  Copyright © 2015年 chinaBond. All rights reserved.
 //
-
+#import <UIKit/UIKit.h>
 #ifndef CBFunction_h
 #define CBFunction_h
 //用户设备系统版本
 #define IOS7_OR_LATER                   ( [[[UIDevice currentDevice] systemVersion] compare:@"7.0"] != NSOrderedAscending )
 #define IOS8_OR_LATER                   ( [[[UIDevice currentDevice] systemVersion] compare:@"8.0"] != NSOrderedAscending )
 
+#define kSystemNavHeight  44.f
+#define kStatusBarH  [[UIApplication sharedApplication] statusBarFrame].size.height
+
+#define JSBNavStatusH                   (kSystemNavHeight+kStatusBarH)
+
+static CGFloat const kTabbarHeight            = 49;
+
+#define JSBViewH   (SCREEN_HEIGHT - JSBNavStatusH - kBottomSafeHeight)
+
+// 状态栏高度
+#define STATUS_BAR_HEIGHT (IS_IPHONE_X_XR ? 44.f : 20.f)
+
+
+/*TabBar高度*/
+#define kTabBarHeight (CGFloat)(IS_IPHONE_X_XR?(49.0 + 34.0):(49.0))
+
+#define kBottomSafeHeight (IS_IPHONE_X_XR ? 34.f:  0.f)
+
+// 判断是否是iPhoneX/Xs
+#define iPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
+/** 判断是否是iPhoneXR/XsMax */
+#define iPhoneXs_Max ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) : NO)
+
+/** 判断是否是iPhoneXR */
+#define isXR ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) : NO)
+/** 用于判断设备是iphoneX以上的设备x,xr,xs,xsMax */
+#define IS_IPHONE_X_XR  iPhoneX||isXR||iPhoneXs_Max
 
 //屏幕宽高
 #define SCREEN_HEIGHT                    (UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation])?([[UIScreen mainScreen] bounds].size.width):([[UIScreen mainScreen] bounds].size.height))
